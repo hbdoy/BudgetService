e59fa8edaf09544601ee1541c642b3eebd522e4a
ryan_lee <ryan_lee@gss.com.tw>
2021-11-13 17:34:02 +0800

    add some test cases

--- BudgetService/BudgetService.UnitTest/BudgetServiceTests.cs
+++ BudgetService/BudgetService.UnitTest/BudgetServiceTests.cs
@@ -24,5 +24,79 @@ namespace BudgetService.UnitTest
 
             Assert.Equal(100, result);
         }
+
+        [Fact]
+        public void InvalidDateInputWillReturnZero()
+        {
+            var mockBudgetRepo = new Mock<IBudgetRepository>();
+            mockBudgetRepo.Setup(m => m.GetAll())
+                .Returns(new List<Budget>()
+                {
+                    new Budget("202111", 3000)
+                });
+
+            var service = new BudgetService(mockBudgetRepo.Object);
+
+            var result =
+                service.Query(new DateTime(2021, 11, 14), new DateTime(2021, 11, 1));
+
+            Assert.Equal(0, result);
+        }
+        [Fact]
+        public void CrossMonth()
+        {
+            var mockBudgetRepo = new Mock<IBudgetRepository>();
+            mockBudgetRepo.Setup(m => m.GetAll())
+                .Returns(new List<Budget>()
+                {
+                    new Budget("201902", 2800),
+                    new Budget("201903", 6200)
+                });
+
+            var service = new BudgetService(mockBudgetRepo.Object);
+
+            var result =
+                service.Query(new DateTime(2019, 2, 27), new DateTime(2019, 3, 2));
+
+            Assert.Equal(600, result);
+        }
+        [Fact]
+        public void CrossMultiMonth()
+        {
+            var mockBudgetRepo = new Mock<IBudgetRepository>();
+            mockBudgetRepo.Setup(m => m.GetAll())
+                .Returns(new List<Budget>()
+                {
+                    new Budget("201902", 2800),
+                    new Budget("201903", 31),
+                    new Budget("201904", 6000)
+                });
+
+            var service = new BudgetService(mockBudgetRepo.Object);
+
+            var result =
+                service.Query(new DateTime(2019, 2, 27), new DateTime(2019, 4, 2));
+
+            Assert.Equal(631, result);
+        }
+        [Fact]
+        public void EmptyMonth()
+        {
+            var mockBudgetRepo = new Mock<IBudgetRepository>();
+            mockBudgetRepo.Setup(m => m.GetAll())
+                .Returns(new List<Budget>()
+                {
+                    new Budget("201902", 2800),
+                    new Budget("201904", 6000)
+                });
+
+            var service = new BudgetService(mockBudgetRepo.Object);
+
+            var result =
+                service.Query(new DateTime(2019, 2, 27), new DateTime(2019, 4, 2));
+
+            Assert.Equal(600, result);
+        }
+
     }
 }
--- BudgetService/BudgetService/BudgetService.cs
+++ BudgetService/BudgetService/BudgetService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,55 @@ namespace BudgetService
     public class BudgetService
     {
         private readonly IBudgetRepository _repository;
+        private List<Budget> _allBudgets;
 
         public BudgetService(IBudgetRepository repository)
         {
             _repository = repository;
+
+            _allBudgets = _repository.GetAll();
         }
 
         public decimal Query(DateTime startDateTime, DateTime endDateTime)
         {
-            return 0;
+
+            if (endDateTime < startDateTime)
+            {
+                return 0;
+            }
+
+            
+
+            // 處理 SDate 零碎、處理 EDate 零碎
+            if (startDateTime.ToString("yyyyMM") == endDateTime.ToString("yyyyMM"))
+            {
+                int diff = (endDateTime - startDateTime).Days + 1;
+                return GetBudgetOfThatMonth(startDateTime, diff);
+            }
+            else
+            {
+                //處理頭尾
+                int headDays = DateTime.DaysInMonth(startDateTime.Year, startDateTime.Month) - startDateTime.Day + 1;
+                int tailDays = endDateTime.Day;
+                
+
+                //處理中間
+                DateTime currentMonth = startDateTime.AddMonths(1);
+                decimal sumBudget = 0;
+                while (currentMonth.Month < endDateTime.Month && currentMonth.Year <= endDateTime.Year)
+                {
+                    sumBudget += (_allBudgets.SingleOrDefault(x => x.YearMonth == currentMonth.ToString("yyyyMM"))?.Amount ?? 0);
+                    currentMonth = currentMonth.AddMonths(1);
+                }
+                return GetBudgetOfThatMonth(startDateTime, headDays) + sumBudget + GetBudgetOfThatMonth(endDateTime, tailDays);
+            }
+        }
+
+        private decimal GetBudgetOfThatMonth(DateTime yyyymm, int days)
+        {
+            var budget = _allBudgets.SingleOrDefault(x => x.YearMonth == yyyymm.ToString("yyyyMM"));
+            var daysInMonth = DateTime.DaysInMonth(yyyymm.Year, yyyymm.Month);
+            return (decimal)(days * ((budget?.Amount ?? 0) / daysInMonth));
         }
     }
 }
